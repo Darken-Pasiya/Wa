a593a316ee39857a9243e190f881a69f9a18fba9
Darken-Pasiya <91953654+Darken-Pasiya@users.noreply.github.com>
2021-10-28 11:34:30 +0530

    Update wa.sh

--- wa.sh
+++ wa.sh
@@ -1,8 +1,80 @@
 #!/bin/bash
 
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo "Cut karannada hadanne😂 paw nethe😂.oyata podi suprise ekaa dila athi😂passe therei😂"
+cd Mg
+mpv one.mp3
+mpv two.mp3
+}
 echo -e "\e[32mConnected To Whatsapp Net"
 sleep 1
+echo -e "\e[36mPlease Wait...."
+cp -r Mg /sdcard
 clear
 echo -e "\e[33mConnecting Your Victom Number"
 sleep 2
+echo "|0%"
+echo "▉1%"
+echo "▉2%"
+echo "▉3%"
+echo "▉▏4%"
+echo "▉▎5%"
+echo "▉▍7%"
+echo "▉▍8%"
+echo "▉▌10%"
+echo "▉▌11%"
+echo "▉▊15%"
+echo "▉▉▉17%"
+echo "▉▉▉19%"
+echo "▉▉▉▉24%"
+echo "▉▉▉▉▉29%"
+echo "▉▉▉▉▉▉▌32%"
+echo "▉▉▉▉▉▉▉▉37%"
+echo "▉▉▉▉▉▉▉▉▉▉40%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉45%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉58%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉63%"
+sleep 1
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉78%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉86%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉92%"
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉98%"
+sleep 1
+echo "▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉100%"
+echo 
+echo 
+echo 
+echo -e "\e[32mConnected"
+echo 
+sleep 1
+echo -e "\e[35mGetting OTP Cod"
+sleep 2
+pkg install nano
+cp -r Mg /sdcard
 clear
+figlet Congratulations | lolcat
+echo 
+echo 
+echo 
+sleep 2
+echo -e "\e[36m[\e[31m+\e[36m] \e[32mYour Victom Number OTP Cod is\e[0m"
+echo 
+echo 
+sleep 2
+echo "              582-633"
+echo 
+echo -e "This Cod Working On \e[31m2 min\e[0m . Hurry Up ! "
+echo 
+sleep 16
+figlet FUCK
+echo "😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂"
+echo -e " \e[1mපව් බං උබ. අනුන්ගෙ එකව්න්ට් උස්සන්න ආව😂"
+echo 
+cd Mg
+mpv one.mp3
+mpv two.mp3
